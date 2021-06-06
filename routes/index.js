9e2f0dcab66645a3e6fe3b2ee16ac16b12a5471c
JoungSik <tjstlr2010@gmail.com>
2021-06-07 08:46:16 +0900

    [Add] pagenation offset 값에 맞춰서 나오도록 수정

--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,36 @@
 import express from "express";
-import db, { query } from "../database";
+import { query } from "../database";
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const city = req.query.city;
   const town = req.query.town;
-  const page = req.query.offset * 10 || 0;
+  const offset = req.query.offset * 10 || 0;
 
   let sql;
   if (city && town) {
-    sql = `SELECT * FROM restaurants WHERE city = '${city}' AND town ='${town}' LIMIT 10 OFFSET ${page}`;
+    sql = `SELECT * FROM restaurants WHERE city = '${city}' AND town ='${town}' LIMIT 10 OFFSET ${offset}`;
   } else {
-    sql = `SELECT * FROM restaurants LIMIT ${page}, 10`;
+    sql = `SELECT * FROM restaurants LIMIT ${offset}, 10`;
   }
-
-  // LIMIT <skip>, <count>
-  console.log(sql);
-
+  
   query(sql,
     (err) => {
       res.status(400).json({ "error": err.message });
     },
     (rows) => {
-
       query('SELECT count(*) as count FROM restaurants', (err) => console.error(err.message),
         (row) => {
-          res.render("index", {
+          const result = {
             isAll: city && town,
             city: city,
             town: town,
-            page: req.query.offset,
+            offset: offset,
             total: row[0].count,
             restaurants: rows,
-          });
+          };
+          res.render("index", result);
         });
     });
 });
